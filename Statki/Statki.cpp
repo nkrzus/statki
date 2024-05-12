608327d6e0aee4d1464ccf420adda5287ec4b8e6
Natalia Krzus <nkrzus95@gmail.com>
2024-05-12 22:40:24 +0200

    Creating 2 function in Statki.cpp to make code more clearly

--- Statki/Statki.cpp
+++ Statki/Statki.cpp
@@ -8,6 +8,7 @@
 #include "HumanPlayer.h"
 #include "ComputerPlayer.h"
 #include "Board.h"
+#include "Statki.h"
 using namespace std;
 
 class Ship {
@@ -22,31 +23,18 @@ public:
 };
 
 
-int main()
+void whoIsTheWinner(bool areHumanPlayerWin, HumanPlayer& player1, bool areComputerPlayerWin, ComputerPlayer& player2)
 {
-	srand(time(0));
-	cout << "GRA W STATKI\n\n";
+	if (areHumanPlayerWin) {
+		cout << "GRATULACJE " << player1.getName() << " - ZWYCIESTWO JEST TWOJE!\n";
+	}
+	else if (areComputerPlayerWin) {
+		cout << "GRATULACJE " << player2.getName() << " - ZWYCIESTWO JEST TWOJE!\n";
+	}
+}
 
-	cout << "Witaj! Podaj swoje imie.\n";
-
-	string playerName;
-	cin >> playerName;
-
-	cout << "Witaj " << playerName << "!\n";
-
-	HumanPlayer player1(playerName);
-
-	player1.addShips();
-	ComputerPlayer player2;
-	player2.addShips();
-
-	//przepisanie tablic przeciwnikow 
-	player1.oponentBoard = player2.getBoard();
-	player2.oponentBoard = player1.getBoard();
-
-	bool areHumanPlayerWin = false;
-	bool areComputerPlayerWin = false;
-
+void playingTheGame(HumanPlayer& player1, bool& areHumanPlayerWin, ComputerPlayer& player2, bool& areComputerPlayerWin)
+{
 	do
 	{
 		player1.guessWhereTheShipsAre();
@@ -56,11 +44,35 @@ int main()
 			areComputerPlayerWin = player2.areYouWinner(player2.playBoard, player2.oponentBoard);
 		}
 	} while (!areComputerPlayerWin && !areHumanPlayerWin);
+}
 
-	if (areHumanPlayerWin) {
-		cout << "GRATULACJE " << player1.getName() << " - ZWYCIESTWO JEST TWOJE!\n";	
-	}
-	else if (areComputerPlayerWin) {
-		cout << "GRATULACJE " << player2.getName() << " - ZWYCIESTWO JEST TWOJE!\n";
-	}
+void game()
+{
+	cout << "Witaj! Podaj swoje imie.\n";
+	string playerName;
+	cin >> playerName;
+	cout << "Witaj " << playerName << "!\n";
+
+	HumanPlayer player1(playerName);
+	ComputerPlayer player2;
+
+	player1.addShips();
+	player2.addShips();
+
+	player1.oponentBoard = player2.getBoard();
+	player2.oponentBoard = player1.getBoard();
+
+	bool areHumanPlayerWin = false;
+	bool areComputerPlayerWin = false;
+
+	playingTheGame(player1, areHumanPlayerWin, player2, areComputerPlayerWin);
+	whoIsTheWinner(areHumanPlayerWin, player1, areComputerPlayerWin, player2);
+}
+
+int main()
+{
+	srand(time(0));
+	cout << "GRA W STATKI\n\n";
+	game();
+	return 0;
 }
